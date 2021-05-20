9ec011320b9c6892e49765c53cdaf07709ca4486
Avichai Yungrais <avichai@iamwebbing.com>
2021-05-20 16:20:35 +0300

    1

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Table_1] (
   [id] [nchar](10) NULL,
   [name] [nchar](10) NULL,
-  [new_col] [nchar](10) NULL
+  [new_col] [nchar](10) NULL,
+  [new] [nchar](10) NULL
 )
 ON [PRIMARY]
 GO

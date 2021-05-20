e028deb2bb8f97ddeeddc069321278a736bd080c
Avichai Yungrais <avichai@iamwebbing.com>
2021-05-20 16:25:59 +0300

    6

--- Tables/StaticData/dbo.TLRM_INV_IMSIIPs.Data.sql
+++ Tables/StaticData/dbo.TLRM_INV_IMSIIPs.Data.sql
@@ -1,1 +1,3 @@
-﻿INSERT dbo.TLRM_INV_IMSIIPs(imsi, ip, apn) VALUES ('321321334', '1.1.1.1', 'ssxkhj')
+﻿
+INSERT dbo.TLRM_INV_IMSIIPs(imsi, ip, apn) VALUES ('321', '1.1.1.1', 'ssxkhj')
+INSERT dbo.TLRM_INV_IMSIIPs(imsi, ip, apn) VALUES ('654654', '127.0.0.1', 'shalom')

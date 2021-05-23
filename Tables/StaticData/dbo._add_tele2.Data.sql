f967079de828255449c89a949a2828c8f4bea908
Avichai Yungrais <avichai@iamwebbing.com>
2021-05-23 09:45:40 +0300

    commit

---
+++ Tables/StaticData/dbo._add_tele2.Data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT dbo._add_tele2 ON
+GO
+INSERT dbo._add_tele2(iccid, id, name, test, test2, test3, test4, test5, test6, test7, test8) VALUES ('654654654654', 2, N'11111', N'12', N'3', N'4', N'5', N'6', N'7', N'8', N'9')
+GO
+SET IDENTITY_INSERT dbo._add_tele2 OFF
+GO
